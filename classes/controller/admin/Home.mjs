f43ed0c9652560b3f4148ae05fb3e12bb499aa61
Colin Leung <colin@cowise.co>
2025-10-04 05:58:30 +0800

    Add log_actions to admin home controller
    Introduces a log_actions set with 'index' action in the ControllerAdminHome constructor to enable action logging for the admin home controller.

--- classes/controller/admin/Home.mjs
+++ classes/controller/admin/Home.mjs
@@ -4,6 +4,7 @@ export default class ControllerAdminHome extends ControllerAdmin {
   constructor(request) {
     super(request, null, {
       roles: new Set(['staff', 'moderator']),
+      log_actions: new Set(['index']),
     });
   }
 
